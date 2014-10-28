f37897f17466070a1aadf692a25ce1cbef7b0064
Quoc-Viet Nguyen <afelion@gmail.com>
2014-10-28 23:18:55 +1000

    Cyclical Redundancy Checking

---
+++ crc_test.go
@@ -0,0 +1,18 @@
+// Copyright 2014 Quoc-Viet Nguyen. All rights reserved.
+// This software may be modified and distributed under the terms
+// of the BSD license.  See the LICENSE file for details.
+package modbus
+
+import (
+	"testing"
+)
+
+func TestCRC(t *testing.T) {
+	var crc crc
+	crc.reset()
+	crc.pushBytes([]byte{0x02, 0x07})
+
+	if 0x4112 != crc.value() {
+		t.Fatalf("crc expected %v, actual %v", 0x4112, crc.value())
+	}
+}
