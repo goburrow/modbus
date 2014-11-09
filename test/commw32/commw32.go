b20f7f5e94bdf8f77679fcbe8676edac41d28c16
Quoc-Viet Nguyen <afelion@gmail.com>
2014-11-09 02:37:39 +0000

    Build constraints for commw32

--- test/commw32/commw32.go
+++ test/commw32/commw32.go
@@ -1,6 +1,7 @@
 // Copyright 2014 Quoc-Viet Nguyen. All rights reserved.
 // This software may be modified and distributed under the terms
 // of the BSD license.  See the LICENSE file for details.
+// +build windows,cgo
 
 // Port of commw32.c
 // To generate go types: go tool cgo commw32.go
